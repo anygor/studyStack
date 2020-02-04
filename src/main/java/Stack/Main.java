07d8a0b9d74493df81befb993b7faba222828031
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2020-02-04 09:13:18 +0300

    FILE OPERATIONS DON'T WORK AND ARE UNSAFE

--- src/main/java/Stack/Main.java
+++ src/main/java/Stack/Main.java
@@ -1,12 +1,64 @@
 package Stack;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        Stack<Double> stack = new Stack<Double>();
-        stack.push(3.5);
-        stack.push(11.0);
-        stack.push(0.0);
-        stack.clear();
-        System.out.println(stack.pop());
+        Stack<Integer> stack = new Stack<Integer>();   // Type is changed here
+        String command;
+        int insert;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This is your stack-management tool");
+        System.out.println("Type help to see avaliable commands");
+        System.out.println("Type quit to finish program");
+
+        loop:
+        while(true) {
+            System.out.println("Digit a command:");
+            command = scanner.nextLine();
+            switch (command) {
+                case "help": {
+                    System.out.println("push - to add a certain number to your stack");
+                    System.out.println("peek - to look at the top for your stack");
+                    System.out.println("pop - to return and remove top of your stack");
+                    System.out.println("takeFromFile - get numbers from data file to your stack");
+                    break;
+                }
+                case "push": {
+                    System.out.println("Digit a number, you want to push:");
+                    insert = scanner.nextInt();
+                    scanner = new Scanner(System.in);
+                    stack.push(insert);
+                    System.out.println("Pushed " + " to your stack");
+                    break;
+                }
+                case "peek": {
+                    System.out.println("Current stack top: " + stack.peek());
+                    break;
+                }
+                case "pop":{
+                    System.out.println("Your removed " + stack.pop() + " from your stack");
+                    break;
+                }
+                case "takeFromFile":{
+                    stack.takeFromFile();
+                    break;
+                }
+                case "putToFile":{
+                    stack.putToFile();
+                }
+                case "quit":{
+                    System.out.println("Fare thee well");
+                    break loop;
+                }
+                default:{
+                    System.out.println("Invalid command, try again");
+                    break;
+                }
+            }
+        }
     }
 }
--- src/main/java/Stack/Stack.java
+++ src/main/java/Stack/Stack.java
@@ -1,8 +1,15 @@
 package Stack;
 
+import java.io.*;
+
 public class Stack<T> {
     private Element<T> top;
     private int size = 0;
+
+
+    BufferedReader in;
+    BufferedWriter out;
+
     private boolean isNotEmpty(){
         return size != 0;
     }
@@ -38,6 +45,37 @@ public class Stack<T> {
     public void clear(){
         while(isNotEmpty()){
             pop();
+        }
+    }
+
+    public void takeFromFile(){
+        try {
+            in = new BufferedReader(new FileReader("src/main/resources/data.txt"));
+            String buffer;
+            while((buffer = in.readLine()) != null)
+                this.push((T) buffer);
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void putToFile(){
+        try{
+            out = new BufferedWriter(new PrintWriter(new FileOutputStream("src/main/resources/data.txt", false)));
+            while(this.isNotEmpty()){
+                out.write((String)this.pop());
+                out.flush();
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
