30a68f51459f45977a4fef50ecf676943b328700
anygor <targemrazor@gmail.com>
2020-05-20 18:00:20 +0300

    Add some tests

--- src/test/java/ListInsertionTests.java
+++ src/test/java/ListInsertionTests.java
@@ -13,4 +13,27 @@ public class ListInsertionTests {
         expected = 10;
         assertEquals(expected, list.popFront());
     }
+
+    @Test
+    public void indexInsertionTest(){
+        LinkedList<String> list = new LinkedList<String>();
+        list.pushFront("0");
+        list.pushBack("2");
+        list.pushAtIndex("1", 1);
+        assertEquals("1", list.peekAtIndex(1));
+    }
+
+    @Test
+    public void indexPushOnlyTest(){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.pushAtIndex(0, 0);
+        list.pushAtIndex(2, 1);
+        list.pushAtIndex(1, 1);
+        Integer expected = 2;
+        assertEquals(expected, list.popBack());
+        expected = 1;
+        assertEquals(expected, list.popBack());
+        expected = 0;
+        assertEquals(expected, list.popBack());
+    }
 }
