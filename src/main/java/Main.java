e9e437b164b7712584b6021a471d2a00ffe2ce5f
anygor <targemrazor@gmail.com>
2020-05-30 15:44:51 +0300

    Add main menu setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,47 @@ import Matrix.MatrixMenu;
 import Queue.QueueMenu;
 import Stack.StackMenu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        MatrixMenu.menu();
+        Scanner input = new Scanner(System.in);
+        System.out.println("1 - Stack");
+        System.out.println("2 - Queue");
+        System.out.println("3 - Linked List");
+        System.out.println("4 - Stream List (tba)");
+        System.out.println("5 - Matrix");
+
+        loop:
+        while (true) {
+            System.out.println("Chose the menu");
+            String command = input.nextLine();
+            switch (command) {
+                case "1":{
+                    StackMenu.menu();
+                    break;
+                }
+                case "2":{
+                    QueueMenu.menu();
+                    break;
+                }
+                case "3":{
+                    LinkedListMenu.menu();
+                    break;
+                }
+                case "5":{
+                    MatrixMenu.menu();
+                    break;
+                }
+                case "quit":{
+                    System.out.println("cya");
+                    break loop;
+                }
+                default:{
+                    System.out.println("Invalid command, try again");
+                    break;
+                }
+            }
+        }
     }
 }
